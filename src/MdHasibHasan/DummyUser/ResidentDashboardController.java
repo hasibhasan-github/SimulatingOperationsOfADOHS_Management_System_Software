a8e5c460caa5186a6e998eac8ee65415f5d4b0df
MD HASIB HASAN <>
2023-08-26 00:47:11 +0600

    Committed by @hasibhasan-github

--- src/MdHasibHasan/DummyUser/ResidentDashboardController.java
+++ src/MdHasibHasan/DummyUser/ResidentDashboardController.java
@@ -4,6 +4,7 @@
  */
 package MdHasibHasan.DummyUser;
 
+import MdHasibHasan.CantonmentBoardMember.dohsPolicies;
 import MdHasibHasan.DataReadWrite;
 import MdHasibHasan.GenerateAlerts;
 import MdHasibHasan.MaintenanceOfficer.carStickerRequest;
@@ -12,6 +13,8 @@ import MdHasibHasan.sceneChanging;
 import MdHasibHasan.signUpData;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -60,18 +63,30 @@ public class ResidentDashboardController implements Initializable {
         maintenanceFeeStatusLabel.setText("Unpaid");
         //System.out.println(currentlyLoggedInUserInfo.getEmail());
         applicationStatusLabel.setText("Not Applied");
-        loadPolicies.setText("Noise Control Policy: Residents must keep noise levels at a reasonable level between 10:00 PM and 8:00 AM to ensure a peaceful living environment.\n" + 
-                                        "Parking Regulation: Residents must use their designated parking spaces only, and visitor parking is strictly reserved for guests." +"\n"+ "Unauthorized parking in visitor spots may result in penalties.\n" +
-                                        "Waste Management Policy: All residents must follow the society's waste disposal guidelines, including separating recyclables and putting \n out trash only during designated collection times.\n" + 
-                                        "Alteration Approval Process: Any structural changes or renovations to units require prior approval from the society management to \n ensure compliance with safety and aesthetic standards.\n" + 
-                                        "Smoking is prohibited in all indoor and outdoor common areas to maintain a healthy and smoke-free environment for all residents. Designated smoking areas may be provided for those who smoke.\n"+
-                                        "Residents are allowed to decorate their balconies and exteriors within specified guidelines. Decorations should not obstruct views, create safety hazards, or adversely affect the overall appearance of the building.\n" + 
-                                        "Usage of amenities such as the gym, pool, and clubhouse should adhere to designated quiet hours. This ensures that residents can enjoy these facilities without disturbing others.");
+        
         maintenceFeeMonthComboBox.getItems().addAll("January", "February", "March", "April", "May",
                 "June", "July", "August", "September", "October", "November", "December");
         maintenceFeeYearComboBox.getItems().addAll("2023", "2024", "2025", "2026", "2027", "2028");
         
         refreshMaintenanceFeeDataList();
+        
+        // Creating Dummy Instance of dohsPolicies.
+        dohsPolicies dummyInstance = new dohsPolicies("", LocalDate.of(2023, 02, 02),
+                                                LocalDate.of(2023, 02, 02), new ArrayList<String>());
+        // Creating Dummy Instance of dohsPolicies.
+        
+        // Reading data from DOHSPOLICIES.bin file.
+        ObservableList<dohsPolicies> policyListOfDohs = (ObservableList<dohsPolicies>) DataReadWrite.readObjectToFile("DOHSPOLICIES.bin", dummyInstance);
+        // Reading data from DOHSPOLICIES.bin file.
+        
+        // Showing the Policies in policyTextArea.
+        for ( dohsPolicies policyData : policyListOfDohs ){
+            loadPolicies.appendText(policyData.getPolicyName() + "\n");
+            for ( int i = 0; i < policyData.getPolicyDescription().size(); ++i ){
+                loadPolicies.appendText(policyData.getPolicyDescription().get(i)+"\n");
+            }
+        }
+        // Showing the Policies in policyTextArea
         
     }
     
@@ -125,13 +140,10 @@ public class ResidentDashboardController implements Initializable {
         // System.out.println(currentlyLoggedInUserInfo.getEmail());
         applied = true;
         applyForCarStickerLabel.setDisable(true);
+        applicationStatusLabel.setText("Pending");
         carStickerRequest application = new carStickerRequest(currentlyLoggedInUserInfo.getId(), "Pending",
                                             currentlyLoggedInUserInfo.getEmail(), "Done");
-        System.out.println(application.getEmail());
         DataReadWrite.writeObjectToFile("CarStickerRequestData.bin", application);
-        
-
-        
     }
 
     @FXML

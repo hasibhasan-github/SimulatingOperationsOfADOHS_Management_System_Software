4ac85a4b203681faa4dfaf7dc65457629fbfbb75
@Abdullah Al Mamun <>
2023-08-25 21:51:37 +0600

    Commited by @abdullah2111

--- src/Abdullah/UtilityServiceManager/UtilityServiceManager.java
+++ src/Abdullah/UtilityServiceManager/UtilityServiceManager.java
@@ -4,10 +4,24 @@
  */
 package Abdullah.UtilityServiceManager;
 
+import MdHasibHasan.Employee;
+import java.io.Serializable;
+import java.time.LocalDate;
+
 /**
  *
  * @author user
  */
-public class UtilityServiceManager {
+public class UtilityServiceManager extends Employee implements Serializable{
+
+    public UtilityServiceManager(LocalDate dateOoJoin, float salary, String department, String designation, int id, String name, String gender, String email, String userType, LocalDate deathOfBirth, long contactNo) {
+        super(dateOoJoin, salary, department, designation, id, name, gender, email, userType, deathOfBirth, contactNo);
+    }
+
+    @Override
+    protected void applyForLeave() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
     
 }
